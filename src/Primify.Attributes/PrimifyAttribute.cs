5546e0e042f10925e92f4dbaccfe8cc918827cb4
Koddek <codedek@outlook.com>
2025-04-01 14:19:51 -0400

    initial commit attributes

---
+++ src/Primify.Attributes/PrimifyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Primify.Attributes;
+
+/// <summary>
+/// Indicates that a record class or readonly record struct should be treated as a value wrapper
+/// that can be automatically serialized to/from its underlying primitive type.
+/// </summary>
+/// <typeparam name="T">The primitive type that this wrapper encapsulates.</typeparam>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+public sealed class PrimifyAttribute<T> : Attribute;
