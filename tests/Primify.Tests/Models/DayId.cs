8e38449b37787f9011c0783b83ab5e7106ddb00f
Koddek <codedek@outlook.com>
2025-05-11 10:01:46 -0400

    add wrappers for all date primitive types

--- tests/Primify.Tests/Models/DayId.cs
+++ tests/Primify.Tests/Models/DayId.cs
@@ -6,7 +6,26 @@ namespace Primify.Tests.Models;
 [Primify<DateOnly>]
 public partial record class DayId
 {
-    [PredefinedValue("0001-01-01")]
-    public static partial DayId Empty {get;}
-    public static DayId Undefined {get;} = new(DateOnly.MinValue);
+    [PredefinedValue("0001-01-01")] public static partial DayId Empty { get; }
+    public static DayId Undefined { get; } = new(DateOnly.MinValue);
+}
+
+[Primify<TimeOnly>]
+public partial record class TimeId
+{
+    [PredefinedValue("00:00:00")] public static partial TimeId Empty { get; }
+}
+
+[Primify<DateTime>]
+public partial record class DateTimeId
+{
+    [PredefinedValue("0001-01-01T00:00:00")]
+    public static partial DateTimeId Empty { get; }
+}
+
+[Primify<DateTimeOffset>]
+public partial record class DateTimeOffsetId
+{
+    [PredefinedValue("0001-01-01T00:00:00+00:00")]
+    public static partial DateTimeOffsetId Empty { get; }
 }
